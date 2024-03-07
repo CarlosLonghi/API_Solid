e53439fa1caa747352c564c29c350869efeeae84
Carlos Longhi <carloslonghi.cl@gmail.com>
2024-03-07 19:43:44 -0300

    build: install and config eslint

--- src/env/index.ts
+++ src/env/index.ts
@@ -8,9 +8,9 @@ const envSchema = z.object({
 
 const _env = envSchema.safeParse(process.env)
 
-if (_env.success === false) {
+if (!_env.success) {
   console.error('Invalid environment variables.', _env.error.format())
-  
+
   throw new Error('Invalid environment variables.')
 }
 
