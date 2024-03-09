e810f6764df9f5b71bc463d14c42f464b8c35db9
Carlos Longhi <carloslonghi.cl@gmail.com>
2024-03-09 14:20:07 -0300

    build: install Prisma ORM

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+void prisma.user.create({
+  data: {
+    name: 'Carlos Longhi',
+    email: 'carlos@gmail.com'
+  }
+})
