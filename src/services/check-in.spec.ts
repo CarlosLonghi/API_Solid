6e3ca35435cf5d9f88989f9243a3c35a2abe964b
Carlos Longhi <carloslonghi.cl@gmail.com>
2024-04-03 00:12:27 -0300

    feat: create check-in service

---
+++ src/services/check-in.spec.ts
@@ -0,0 +1,24 @@
+import { expect, describe, it, beforeEach } from 'vitest'
+import { InMemoryCheckInsRepository } from '@/repositories/in-memory/in-memory-check-ins-repository'
+import { CheckInService } from './check-in'
+
+// Unit Tests
+
+let checkInsRepository: InMemoryCheckInsRepository
+let checkInService: CheckInService
+
+describe('Check-in Service', () => {
+  beforeEach(() => {
+    checkInsRepository = new InMemoryCheckInsRepository()
+    checkInService = new CheckInService(checkInsRepository)
+  })
+
+  it('should be able to check-in', async () => {
+    const { checkIn } = await checkInService.execute({
+      gymId: 'gym-01',
+      userId: 'user-01'
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+})
---
+++ src/services/check-in.ts
@@ -0,0 +1,28 @@
+import { type CheckIn } from '@prisma/client'
+import { type CheckInsRepository } from '@/repositories/check-ins-repository'
+
+interface CheckInServiceRequest {
+  userId: string
+  gymId: string
+}
+
+interface CheckInServiceResponse {
+  checkIn: CheckIn
+}
+
+export class CheckInService {
+  constructor (
+    private readonly checkInsRepository: CheckInsRepository
+  ) {}
+
+  async execute ({ userId, gymId }: CheckInServiceRequest): Promise<CheckInServiceResponse> {
+    const checkIn = await this.checkInsRepository.create({
+      user_id: userId,
+      gym_id: gymId
+    })
+
+    return {
+      checkIn
+    }
+  }
+}
