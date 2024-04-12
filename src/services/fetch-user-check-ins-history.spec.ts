ad89c519eddc138edac14b866315fd5d0dbab50b
Carlos Longhi <carloslonghi.cl@gmail.com>
2024-04-11 23:18:55 -0300

    test: create test to paginate to user chec k-ins history

--- src/services/fetch-user-check-ins-history.spec.ts
+++ src/services/fetch-user-check-ins-history.spec.ts
@@ -25,13 +25,34 @@ describe('Fetch User Check-in History Service', () => {
     })
 
     const { checkIns } = await fetchUserCheckInsHistoryService.execute({
-      userId: 'user-01'
+      userId: 'user-01',
+      page: 1
     })
 
     expect(checkIns).toHaveLength(2)
     expect(checkIns).toEqual([
       expect.objectContaining({ gym_id: 'gym-01' }),
       expect.objectContaining({ gym_id: 'gym-02' })
+    ])
+  })
+
+  it('should be able to fetch paginated user check-ins history', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await checkInsRepository.create({
+        gym_id: `gym-${i}`,
+        user_id: 'user-01'
+      })
+    }
+
+    const { checkIns } = await fetchUserCheckInsHistoryService.execute({
+      userId: 'user-01',
+      page: 2
+    })
+
+    expect(checkIns).toHaveLength(2)
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: 'gym-21' }),
+      expect.objectContaining({ gym_id: 'gym-22' })
     ])
   })
 })
