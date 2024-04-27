048e425b8403447ac9e42e9116b578ca796a2cad
Carlos Longhi <carloslonghi.cl@gmail.com>
2024-04-27 11:38:04 -0300

    feat: add service to fetch nearby gyms

--- src/repositories/in-memory/in-memory-gyms-repository.ts
+++ src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -1,6 +1,7 @@
 import { Prisma, type Gym } from '@prisma/client'
-import { type GymsRepository } from '../gyms-repository'
+import { type FindManyNearbyParams, type GymsRepository } from '../gyms-repository'
 import { randomUUID } from 'crypto'
+import { getDistanceBetweenCoordinates } from '@/utils/get-distance-between-coordinates'
 
 export class InMemoryGymsRepository implements GymsRepository {
   public items: Gym[] = []
@@ -13,6 +14,25 @@ export class InMemoryGymsRepository implements GymsRepository {
     }
 
     return gym
+  }
+
+  async findManyNearby (params: FindManyNearbyParams) {
+    return this.items.filter(item => {
+      const distance = getDistanceBetweenCoordinates(
+        {
+          latitude: params.latitude,
+          longitude: params.longitude
+        },
+        {
+          latitude: item.latitude.toNumber(),
+          longitude: item.longitude.toNumber()
+        }
+      )
+
+      console.log(distance)
+
+      return distance < 10
+    })
   }
 
   async searchMany (query: string, page: number) {
